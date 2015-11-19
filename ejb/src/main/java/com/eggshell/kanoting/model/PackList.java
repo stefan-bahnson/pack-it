13c7f75c307924d9038a193a758ebbcbd61b5db1
Stefan Bahnson <stefanbahnson@gmail.com>
2015-11-20 00:21:59 +0100

    code clean up packlist

--- ejb/src/main/java/com/eggshell/kanoting/model/PackList.java
+++ ejb/src/main/java/com/eggshell/kanoting/model/PackList.java
@@ -7,10 +7,10 @@ import javax.persistence.*;
     @NamedQuery(name = "PackList.findALL",
             query = "SELECT pl FROM PackList pl"),
     @NamedQuery(name = "PackList.findPackListByUserId",
-            query = "SELECT pl FROM PackList pl WHERE pl.user.id = :userId")
-
+            query = "SELECT pl FROM PackList pl WHERE pl.user.id = :userId"),
+    @NamedQuery(name="PackList.findByUsers",
+            query="Select p FROM PackList p WHERE p.user.id = :userId")
 })
 @Entity
 @Table(name = "packlist")
-@NamedQuery(name="PackList.findByUsers", query="Select p FROM PackList p WHERE p.user.id = :userId")
 public class PackList extends ListEntity {}
